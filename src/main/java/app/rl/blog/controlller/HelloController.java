672b2b8acdfa3dddebeaaeab9d014cf566b38a4c
Ronan Lenouvel <ronan.develop@gmail.com>
2023-08-07 19:10:02 +0200

    app : HelloWorld

---
+++ src/main/java/app/rl/blog/controlller/HelloController.java
@@ -0,0 +1,14 @@
+package app.rl.blog.controlller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String helloWorld() {
+
+        return "Welcome to my first site";
+    }
+}
