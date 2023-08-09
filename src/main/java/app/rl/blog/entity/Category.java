c2675326769f46f00f77b9c7c1221b4eaebb22c5
Ronan Lenouvel <ronan.develop@gmail.com>
2023-08-09 14:00:13 +0200

    Lombok : use possibility to remove boilerplate code

--- src/main/java/app/rl/blog/entity/Category.java
+++ src/main/java/app/rl/blog/entity/Category.java
@@ -6,8 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Category {
     
     @Id
@@ -18,58 +26,5 @@ public class Category {
     protected String slug;
     @Lob
     @Column(columnDefinition = "TEXT")
-    protected String description;
-
-    public Category() {
-    }
-
-    public Category(Long id, String title, String slug, String description) {
-
-        this.id = id;
-        this.title = title;
-        this.slug = slug;
-        this.description = description;
-    }
-
-
-    public Long getId() {
-
-        return this.id;
-    }
-
-    public void setId(Long id) {
-
-        this.id = id;
-    }
-
-    public String getTitle() {
-        
-        return this.title;
-
-    }
-
-    public void setTitle(String title) {
-
-        this.title = title;
-    }
-
-    public String getSlug() {
-
-        return this.slug;
-    }
-
-    public void setSlug(String slug) {
-
-        this.slug = slug;
-    }
-
-    public String getDescription() {
-
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-
-        this.description = description;
-    }
+    protected String description;    
 }
