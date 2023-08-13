8cdbb0acefaa084e0d408a9034d548bbb86289a1
Ronan Lenouvel <ronan.develop@gmail.com>
2023-08-13 15:47:42 +0200

    Refacto : CategoryRepository_FindByID_ReturnCategory using the repository to simulate the database & @BeforeEach to instanciate Catergory

--- src/test/java/app/rl/blog/repository/CategoryRepositoryTests.java
+++ src/test/java/app/rl/blog/repository/CategoryRepositoryTests.java
@@ -3,6 +3,7 @@ package app.rl.blog.repository;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import app.rl.blog.entity.Category;
+import jakarta.persistence.EntityManager;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -18,11 +20,25 @@ public class CategoryRepositoryTests {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void setup() {
+
+        Category category = Category.builder()
+                        .title("title")
+                        .slug("title")
+                        .description("description")
+                        .build();
+
+        entityManager.persist(category);
+    }
+
     @Test
     public void CategoryRepository_SaveAll_ReturnSavedCategory() {
 
         Category category = Category.builder()
-                .id(1L)
                 .title("title")
                 .description("description")
                 .slug("title").build();
@@ -37,14 +53,12 @@ public class CategoryRepositoryTests {
     public void CategoryRepository_GetAll_ReturnMoreThanOneCategory() {
 
         Category category1 = Category.builder()
-                .id(1L)
                 .title("category-1")
                 .slug("category-1")
                 .description("description")
                 .build();
         
         Category category2 = Category.builder()
-                .id(2L)
                 .title("titre-2")
                 .slug("titre-2")
                 .description("description")
@@ -61,19 +75,9 @@ public class CategoryRepositoryTests {
     @Test
     public void CategoryRepository_FindByID_ReturnCategory() {
 
-        Category category = Category.builder()
-                .id(1L)
-                .title("title")
-                .slug("title")
-                .description("description")
-                .build();
+        Category savedCategory = categoryRepository.findById(1L).get();
 
-        categoryRepository.save(category);
-
-        Category savedCategory = categoryRepository.findById(category.getId()).get();
-
-        Assertions.assertThat(savedCategory.getId()).isNotNull();
-        Assertions.assertThat(savedCategory.getId()).isSameAs(category.getId());
+        Assertions.assertThat(savedCategory.getId()).isEqualTo(1L);
     }
 
     @Test
