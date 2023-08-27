b447fe84bd85b7110cc08bfc7a448a11d4717433
Ronan Lenouvel <ronan.develop@gmail.com>
2023-08-27 10:32:44 +0200

    Vscode : Opening changes, no modifications

--- src/main/java/app/rl/blog/controller/CategoryController.java
+++ src/main/java/app/rl/blog/controller/CategoryController.java
@@ -1,4 +1,4 @@
-package app.rl.blog.controlller;
+package app.rl.blog.controller;
 
 import java.util.List;
 
@@ -6,13 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import app.rl.blog.entity.Category;
 import app.rl.blog.error.CategoryNotFoundException;
--- src/main/java/app/rl/blog/controller/HelloController.java
+++ src/main/java/app/rl/blog/controller/HelloController.java
@@ -1,4 +1,4 @@
-package app.rl.blog.controlller;
+package app.rl.blog.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
