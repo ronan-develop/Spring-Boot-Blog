c797cff5dcedd5bbbe4df19d76b08e140362a5bd
Ronan Lenouvel <ronan.develop@gmail.com>
2023-08-28 18:16:19 +0200

    Post : date generated when created Closes #49

--- src/main/java/app/rl/blog/entity/Post.java
+++ src/main/java/app/rl/blog/entity/Post.java
@@ -1,11 +1,11 @@
 package app.rl.blog.entity;
 
 import java.sql.Date;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.CreationTimestamp;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,24 +20,24 @@ import lombok.NoArgsConstructor;
 public class Post {
 
     @Id
-    @GeneratedValue(
-        strategy = GenerationType.SEQUENCE,
-        generator = "post_generator"
-        )
+    @GeneratedValue
     private Long id;
 
+    @CreationTimestamp
+    @JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
     @Column(name = "createdAt")
     private Date createdAt;
 
+    @NotBlank(message = "Please add a title")
     private String title;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "categoryId", referencedColumnName = "id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "category_id")
     private Category category;
 
     @Lob
     @Column(columnDefinition = "TEXT")
+    @NotBlank(message = "Please add a content")
     private String content;
     private String slug;
 }
