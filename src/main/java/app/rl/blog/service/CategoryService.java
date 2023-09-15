1f6a0ec868949f329758d9bd050f0cb990a2869c
Ronan Lenouvel <ronan.develop@gmail.com>
2023-09-15 11:01:02 +0200

    Refactor : implements all DAO method in service

--- src/main/java/app/rl/blog/service/CategoryService.java
+++ src/main/java/app/rl/blog/service/CategoryService.java
@@ -5,9 +5,22 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import app.rl.blog.entity.Category;
+import app.rl.blog.error.CategoryNotFoundException;
 
 @Service
 public interface CategoryService {
 
+    public Category saveCategory(Category category);
+
     public List<Category> fetchCategorylist();
+
+    public Category fetchCategoryById(Long id) throws CategoryNotFoundException;
+
+    public Category updateCategory(Long id, Category category);
+
+    public void deleteCategoryById(Long id);
+
+    public Category fetchCategoryByTitle(String title);
+
+    public Category fetchCategoryByTitleIgnoreCase(String title);
 }
--- src/main/java/app/rl/blog/service/CategoryServiceImpl.java
+++ src/main/java/app/rl/blog/service/CategoryServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import app.rl.blog.dao.CategoryDAO;
 import app.rl.blog.entity.Category;
+import app.rl.blog.error.CategoryNotFoundException;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -21,6 +22,42 @@ public class CategoryServiceImpl implements CategoryService {
     public List<Category> fetchCategorylist() {
 
         return categoryDAO.fetchCategorylist();
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        
+        return categoryDAO.saveCategory(category);
+    }
+
+    @Override
+    public Category fetchCategoryById(Long id) throws CategoryNotFoundException {
+
+        return categoryDAO.fetchCategoryById(id);
+    }
+
+    @Override
+    public Category updateCategory(Long id, Category category) {
+        
+        return categoryDAO.updateCategory(id, category);
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+        
+        categoryDAO.deleteCategoryById(id);
+    }
+
+    @Override
+    public Category fetchCategoryByTitle(String title) {
+
+        return categoryDAO.fetchCategoryByTitle(title);
+    }
+
+    @Override
+    public Category fetchCategoryByTitleIgnoreCase(String title) {
+        
+        return categoryDAO.fetchCategoryByTitleIgnoreCase(title);
     }
 
 
